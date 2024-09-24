f507c38034260908b11956d094f09aef5967bc57
mansour-9 <126260945+mansour-9@users.noreply.github.com>
2024-09-24 23:57:50 +0300

    Update LinkedList.java

--- src/main/java/com/example/project/LinkedList.java
+++ src/main/java/com/example/project/LinkedList.java
@@ -55,12 +55,29 @@ public class LinkedList<T> implements List<T>{
         }
     }
     public T mostFrequentElement() {
-        throw new UnsupportedOperationException("Not supported yet.");
-        //         Write the method mostFrequentElement, member of the class LinkedList, that returns
-        // the most frequent element in the list. The most frequent element is the element
-        // appearing the highest number of times. If one or more element appear the same
-        // number of times, the one encountered earlier is returned.
-        // Example 1.1. Given the list l : A, B, C, B, C, D, E, mostFrequentElement() returns
-        // B.
+        int countA=0,countB=0,countC=0,countD=0,countE=0;
+        Node<T> temp=head;
+        while(temp.next!=null) {
+            switch (((char)temp.data)) {
+                case 'A':countA++;
+                case 'B':countB++;
+                case 'C':countC++;
+                case 'D':countD++;
+                case 'E':countE++;
+                default:break;
+            }
+        }
+
+        if (countB > countA && countB >= countC && countB >= countD && countB >= countE) {
+            return (T) Character.valueOf('B');
+        } else if (countC > countA && countC > countB && countC >= countD && countC >= countE) {
+            return (T) Character.valueOf('C');
+        } else if (countA >= countB && countA >= countC && countA >= countD && countA >= countE) {
+            return (T) Character.valueOf('A');
+        } else if (countD >= countA && countD >= countB && countD >= countC && countD >= countE) {
+            return (T) Character.valueOf('D');
+        } else {
+            return (T) Character.valueOf('E');
+        }
     }
 }
