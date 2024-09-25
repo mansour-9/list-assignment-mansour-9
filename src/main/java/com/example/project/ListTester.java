9dfc44610193a722a31c16906d1650a877400d0c
mansour-9 <126260945+mansour-9@users.noreply.github.com>
2024-09-25 04:28:50 +0300

    Update ListTester.java

--- src/main/java/com/example/project/ListTester.java
+++ src/main/java/com/example/project/ListTester.java
@@ -1,31 +1,41 @@
 package com.example.project;
 
-public class ListTester {
-    public static <T> void circularLeftShift(List <T> list, int n)
-    {
-        for(int i=0;i<n;i++){
-           list.findFirst();
-           T temp=list.retrieve();
-           int size=0;
-           while (!list.last()){
-               size++;
-               list.findNext();
-           }
-           for(int j=0;j<size-1;j++) {
-               list.findNext();
-               list.insert(list.retrieve());
-           }
-           list.insert(temp);
-       }
+public static <T> void circularLeftShift(DLL<T> list, int n) {
+    if (list.empty()) {
+        return; 
     }
-    public static <T> void reverseCopy(DLL<T> l1, DLL<T> l2)
-    {
-        DoubleLinkedList<T> temp = l1;
-        temp.findLast();
-        while (temp.retrieve() != null) {
-            l2.insert(temp.retrieve());
-            temp.findPrevious();
-
-        }
+    int size = 0;
+    list.findFirst();
+    while (!list.last()) {
+        size++;
+        list.findNext();
+    }
+    size++; 
+    n = n % size;
+    if (n == 0) {
+        return; 
+    }
+    for (int i = 0; i < n; i++) {
+        list.findFirst(); 
+        T firstElement = list.retrieve(); 
+        for (int j = 0; j < size - 1; j++) {
+            list.update(list.retrieveNext()); 
+            list.findNext();
+        }      
+        list.update(firstElement);
     }
 }
+   public static <T> void reverseCopy(DLL<T> l1, DLL<T> l2) {
+    if (l1.empty()) 
+        return;
+    l1.findLast();
+    while (true) {
+        l2.insert(l1.retrieve());
+        if (l1.first()) {
+            break;
+        }   
+        l1.findPrevious();
+    }
+}
+
+
