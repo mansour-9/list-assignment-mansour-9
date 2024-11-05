689fb671ea2158ee9ff5cca1b27f3e9ccb80d571
mansour-9 <126260945+mansour-9@users.noreply.github.com>
2024-11-06 00:26:25 +0300

    Update LinkedList.java

--- src/main/java/com/example/project/LinkedList.java
+++ src/main/java/com/example/project/LinkedList.java
@@ -1,33 +1,41 @@
 package com.example.project;
 
-public class LinkedList<T> implements List<T>{
-	private Node<T> head;
-	private Node<T> current;
-	
+public class LinkedList<T> implements List<T> {
+    private Node<T> head;
+    private Node<T> current;
+
     public LinkedList() {
-		head = current = null;
-	}
-	public boolean empty() {
-		return head == null;
-	}
-	public boolean last() {
-		return current.next == null;
-	}
+        head = current = null;
+    }
+
+    public boolean empty() {
+        return head == null;
+    }
+
+    public boolean last() {
+        return current.next == null;
+    }
+
     public boolean full() {
         return false;
     }
+
     public void findFirst() {
         current = head;
     }
+
     public void findNext() {
         current = current.next;
     }
+
     public T retrieve() {
         return current.data;
     }
+
     public void update(T e) {
         current.data = e;
     }
+
     public void insert(T e) {
         if (empty()) {
             current = head = new Node<T>(e);
@@ -38,6 +46,7 @@ public class LinkedList<T> implements List<T>{
             current.next = tmp;
         }
     }
+
     public void remove() {
         if (current == head) {
             head = head.next;
@@ -54,31 +63,27 @@ public class LinkedList<T> implements List<T>{
             current = current.next;
         }
     }
-	
-return mfe ;
-}
-  public T mostFrequentElement() {
-    T mfe = null;
-    int max = 0;
-    Node<T> p = head;
-    while (p != null) {
-        Node<T> q = p;
-        int count = 0;
-        while (q != null) {
-            if (q.data.equals(p.data))
-                count++;
-            q = q.next;
+
+    public T mostFrequentElement() {
+        T mfe = null;
+        int max = 0;
+        Node<T> p = head;
+        while (p != null) {
+            Node<T> q = p;
+            int count = 0;
+            while (q != null) {
+                if (q.data.equals(p.data))
+                    count++;
+                q = q.next;
+            }
+            if (count > max) {
+                max = count;
+                mfe = p.data;
+            }
+            p = p.next;
         }
-        if (count > max) {
-            max = count;
-            mfe = p.data;
-        }
-        p = p.next;
+        return mfe;
     }
-    return mfe;
-}
-
-
 
 
 }
