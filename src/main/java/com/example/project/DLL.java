ad0e0a421e5f9e06472edf66d8b99af6677dc135
mansour-9 <126260945+mansour-9@users.noreply.github.com>
2024-11-06 00:24:40 +0300

    Update DLL.java

--- src/main/java/com/example/project/DLL.java
+++ src/main/java/com/example/project/DLL.java
@@ -69,6 +69,77 @@ public class DLL<T> {
         else
             current = current.next;
     }
+ import java.util.Comparator;
+
+public class DLL<T> {
+    private DLLNode<T> head;
+    private DLLNode<T> current;
+    private Comparator<T> comparator;
+
+    public DLL() {
+        head = current = null;
+    }
+    public DLL(Comparator<T> comparator) {
+        head = current = null;
+        this.comparator = comparator;
+    }
+    public boolean empty() {
+        return head == null;
+    }
+    public boolean last() {
+        return current.next == null;
+    }
+    public boolean first() {
+        return current.previous == null;
+    }
+    public boolean full() {
+        return false;
+    }
+    public void findFirst() {
+        current = head;
+    }
+    public void findNext() {
+        current = current.next;
+    }
+    public void findPrevious() {
+        current = current.previous;
+    }
+    public T retrieve() {
+        return current.data;
+    }
+    public void update(T val) {
+        current.data = val;
+    }
+    public void insert(T val) {
+        DLLNode<T> tmp = new DLLNode<T>(val);
+        if(empty()) {
+            current = head = tmp;
+        }
+        else {
+            tmp.next = current.next;
+            tmp.previous = current;
+            if(current.next != null)
+                current.next.previous = tmp;
+            current.next = tmp;
+            current = tmp;
+        }
+    }
+    public void remove() {
+        if(current == head) {
+            head = head.next;
+            if(head != null)
+                head.previous = null;
+        }
+        else {
+            current.previous.next = current.next;
+            if(current.next != null)
+                current.next.previous = current.previous;
+        }
+        if(current.next == null)
+            current = head;
+        else
+            current = current.next;
+    }
   public  void removeBetween ( T e1 , T e2 ) {
             Node <T> p = head ;
        while (( p != null ) && (! p.data.equals( e1 )))
@@ -84,4 +155,6 @@ public class DLL<T> {
                     q.previous = p ;
                        current = head ;
 }
+}
+
 }
